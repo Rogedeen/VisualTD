f4772a42681d7380b08761a3835f9b63b8cfe11b
Rogedeen <ynsisikgoz@gmail.com>
2026-05-21 17:06:22 +0300

    Cleanup: Removed unused fields in MageAI.cs to clear compiler warnings

--- Assets/Scripts/MageAI.cs
+++ Assets/Scripts/MageAI.cs
@@ -3,13 +3,8 @@ using UnityEngine;
 public class MageAI : MonoBehaviour
 {
     [Header("Mage Settings")]
-    [SerializeField] private float attackRange = 15f;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject fireballPrefab;
-
-    [Header("Fortify Settings")]
-    [SerializeField] private float fortifyRange = 10f;
-    [SerializeField] private float fortifyAmount = 100f;
 
     private Animator animator;
     private Transform currentTarget;
